f7c182ab42203aa4556f0bb3361b9d426bdd60cc
Anna <kravetsanna78@gmail.com>
2022-01-27 14:30:40 +0200

    first practice

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,22 @@
 
+const numberOfFilms = prompt("How many movies did you see?", " ");
+//console.log(numberOfFilms);
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const filmFirst = prompt("What movie did you see the last time?", ""),
+      filmReview1 = prompt("How do you rade it?", ""),
+      filmSecond = prompt("What movie did you see the last time?", ""),
+      filmReview2 = prompt("How do you rade it?", "");
+
+personalMovieDB.movies[filmFirst] = filmReview1;
+personalMovieDB.movies[filmSecond] = filmReview2;
+
+console.log(personalMovieDB);
